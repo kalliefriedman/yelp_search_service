d7e668c6e9b0423cccb277202f975ba65e557b25
vagrant <vagrant@vagrant.vm>
2017-06-04 15:39:04 +0000

    importing configparser

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-# Your goal is to help evaluate if public consumer review 
-# and rating data could be incorporated to improve consumer 
-# decision making. Specifically, we'd like you to write a 
-# basic wrapper for the Yelp API that searches for a 
-# specific entity (doctor name and address) and 
-# returns relevant data.
-
-# + Return search result json if there is a search match. 
-# If no match is found, how would you propose a soft fallback solution? 
-# (no need to implement fallback).
-# + Place any keys in a config.ini and import using ConfigParser
-
+import ConfigParser
 from os import environ
 from flask import (Flask, request,
 from requests import                    jsonify)
 
 app = Flask(__name__)
+
 
 
 # starting app
