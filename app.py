7f186df6e8bf8910d8714b37c861cedaf52c26a4
vagrant <vagrant@vagrant.vm>
2017-06-06 00:01:06 +0000

    progress to date

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import ConfigParser
 from os import environ
 from flask import (Flask, request, jsonify)
-from requests import *
+import requests
 import views
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -10,6 +10,25 @@ app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
+
+SEARCH_URL = 'https://api.yelp.com/v3/businesses/search'
+
+
+@app.route('/search-result/<term><location>')
+def make_api_request(term, location):
+    """takes in a token, gets docname and address from form, and returns response"""
+    Config = ConfigParser.ConfigParser()
+    Config.read('config.ini')
+    cred_dict = {}
+    for section_name in Config.sections():
+        for name, value in Config.items(section_name):
+            cred_dict[name] = value
+    url_params = {"term": term, "location": location}
+    bearer_token = cred_dict.get("bearer_token")
+    print bearer_token
+    headers = {'Authorization': 'Bearer ' + bearer_token}
+    response = requests.request('GET', SEARCH_URL, headers=headers, params=url_params)
+    return response.json()
 
 # starting app
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,19 +1,3 @@
-import ConfigParser
-from app import *
-
-SEARCH_URL = 'https://api.yelp.com/v3/businesses/search'
 
 
-@app.route('/search_result', methods=["GET"])
-def make_api_request():
-    """takes in a token, gets docname and address from form, and returns response"""
-    Config = ConfigParser.ConfigParser()
-    Config.read("\config.ini")
-    print Config.sections()
-    # docname = request.args.get("term")
-    # address = request.args.get("location")
-    # url_params = {"term": docname, "location": address}
-    # headers = {'Authorization': 'Bearer ' + bearer_token}
-    # response = requests.request('GET', SEARCH_URL, headers=headers, params=url_params)
-    # return response.json()
-    return 'OK'
+
